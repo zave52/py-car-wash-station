209c6bac5023c13629e55c6f00b9929e6ee449c6
zakhar <zaharsavchen@gmail.com>
2024-06-22 19:23:31 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self, comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
+
 
 class CarWashStation:
     def __init__(self,
                  distance_from_city_center: float,
                  clean_power: int,
                  average_rating: float = 0,
-                 count_of_ratings: int = 0
-    ) -> None:
+                 count_of_ratings: int = 0) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -26,15 +28,17 @@ class CarWashStation:
     def calculate_washing_price(self, car: Car) -> float:
         if self.clean_power < car.clean_mark:
             return 0
-        return round(car.comfort_class *
-                     (self.clean_power - car.clean_mark) *
-                     self.average_rating /
-                     self.distance_from_city_center, 1)
+        return round(car.comfort_class * (self.clean_power - car.clean_mark)
+                     * self.average_rating
+                     / self.distance_from_city_center, 1)
 
     def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
     def rate_service(self, mark: int) -> None:
-        self.average_rating = round((self.average_rating * self.count_of_ratings + mark) / (self.count_of_ratings + 1), 1)
+        average_rating = round((self.average_rating
+                                * self.count_of_ratings + mark)
+                               / (self.count_of_ratings + 1), 1)
+        self.average_rating = average_rating
         self.count_of_ratings += 1
